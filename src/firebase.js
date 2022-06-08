9b5eb3663ff178faa6cdad08eaa422b30615a3c6
ogunye tofunmi <ogunyeteeholuwar@gmail.com>
2022-06-07 12:04:19 -1200

    Adding missing codes

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAp-2Tnt11Hvuvxr0-Xwa6DGmPWPVta4oE",
+    authDomain: "my-hertech.firebaseapp.com",
+    projectId: "my-hertech",
+    storageBucket: "my-hertech.appspot.com",
+    messagingSenderId: "977889467718",
+    appId: "1:977889467718:web:408b92c122790459c21947",
+    measurementId: "G-245NGS9N3H"
+  };
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export { db };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { Link } from 'react-router-dom'
+import { db } from "../firebase";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+
+
+
 
 export default function Login() {
-  
+  const[passwordSwitch, setPasswordSwitch]=useState(false);
+  const [password, setPassword] = useState("");
+const [email, setEmail] = useState("");
+
+const  Login = async (e) => {
+  e.preventDefault();
+  try{
+    await addDoc(collection(db, "tasks"), {
+     email:email,
+     password:password,
+     created:Timestamp.now(),
+    });
+  } catch (err){
+    alert(err);
+  }
+  };
+
+
+
   return (
     <div className='w-full h-auto flex justify-center items-center bg-gray-200'>
       <div className="w-96 h-auto bg-white shadow-md rounded-xl p-6 flex flex-col mt-16">
           <h3 className='text-black text-2xl font-bold text-center'>HerTech</h3>
           <p className='text-gray-600 text-md font-light text-center mt-2'>Welcome back!</p>
 
-          <input type="email" placeholder="email address" className='w-full h-10 rounded-lg border-2 border-gray-500 px-6 mt-4' />
-          <input type="password" placeholder="password" className='w-full h-10 rounded-lg border-2 border-gray-500 px-6 mt-4 show' />
+          <input
+           type="email"
+            placeholder="email address" 
+            className='w-full h-10 rounded-lg border-2 border-gray-500 px-6 mt-4'
+            onChange={(e) => setEmail(e.target.value)}
 
+             />
+             <div className="relative">
+          <input
+           type={passwordSwitch ? "text" : "password"} 
+           placeholder="password" 
+           className='w-full h-10 rounded-lg border-2 border-gray-500 px-6 mt-4'
+           
+           onChange={(e) => setPassword(e.target.value)}
+           />
+         <span
+          className='text-right absolute right-4 bottom-0 top-6 left-8 text-gray-500 font-x5 cursor-pointer'
+           onClick={() => setPasswordSwitch(!passwordSwitch)}
+           >
+           show
+         </span>
+         </div>
           <button className='w-full h-12 rounded-lg bg-black text-white font-bold text-md mt-5'>Login to HerTech</button>
 
           <p className=' text-center mt-6 font-light text-2xl text-gray-600'>OR</p>
